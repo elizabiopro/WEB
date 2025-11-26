70561d6fa618b9e5a3e1864558934f9edfc96e69
elizabiopro <elizabiopro@gmail.com>
2025-11-26 06:47:48 +0530

    Update inheritance.php

--- WT_External/Exip_8/Inheritance/inheritance.php
+++ WT_External/Exip_8/Inheritance/inheritance.php
@@ -1,1 +1,18 @@
+<?php
+class Animal {
+    public function eat() {
+        echo "Animal eats<br>";
+    }
+}
+
+class Dog extends Animal {
+    public function bark() {
+        echo "Dog barks<br>";
+    }
+}
+
+$d = new Dog();
+$d->eat();   // from parent
+$d->bark();  // from child
+?>
 
