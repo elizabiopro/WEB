fd21961f8c0fd5fef87327f202bfd33ff7083c3b
elizabiopro <elizabiopro@gmail.com>
2025-11-26 06:38:42 +0530

    Create hello.php

---
+++ WT_External/Exip_6_HelloWorld using php/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World Page</title>
+</head>
+<body>
+    <h1>My First PHP + HTML Page</h1>
+
+    <?php
+        // PHP prints Hello World
+        echo "<p>Hello World! This is my first PHP web page.</p>";
+    ?>
+</body>
+</html>
