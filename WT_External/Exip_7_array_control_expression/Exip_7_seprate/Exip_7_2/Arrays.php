5a6de9a91bb07ad514a6c7711e577b3f857c8e25
elizabiopro <elizabiopro@gmail.com>
2025-11-26 06:43:51 +0530

    Create Arrays.php

---
+++ WT_External/Exip_7_array_control_expression/Exip_7_seprate/Exip_7_2/Arrays.php
@@ -0,0 +1,12 @@
+//Program of array <br><br>
+<?php
+// Array of fruits
+$fruits = array("Apple", "Banana", "Mango");
+
+// Display elements
+echo "First fruit: " . $fruits[0] . "<br>";
+echo "Second fruit: " . $fruits[1] . "<br>";
+echo "Third fruit: " . $fruits[2];
+?>
+
+
